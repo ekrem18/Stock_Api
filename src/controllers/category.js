b8c60d126b562552ce18fe72201f5be49397b08f
Ekrem <avekremyilmazturk@gmail.com>
2023-11-03 13:29:12 +0300

    uupp

--- src/controllers/category.js
+++ src/controllers/category.js
@@ -1,7 +1,5 @@
 "use strict"
-/* -------------------------------------------------------
-    NODEJS EXPRESS | CLARUSWAY FullStack Team
-------------------------------------------------------- */
+/* -------------------------------------------------------*/
 // Category Controller:
 
 const Category = require('../models/category')
