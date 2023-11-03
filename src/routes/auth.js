59bb3309894cbbaf6cc95c313acb113e40f23c08
Ekrem <avekremyilmazturk@gmail.com>
2023-11-03 15:11:50 +0300

    uuuppp

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -9,7 +9,7 @@ const auth = require('../controllers/auth')
 // URL: /auth
 
 router.post('/login', auth.login)
-router.get('/logout', auth.logout)
+router.get('/logout', auth.logout)                              //---> all metodunu kullanabilecekken yazmıyor oluşumuzun nedeni swagger all'u görmüyor
 router.post('/logout', auth.logout)
 
 /* ------------------------------------------------------- */
